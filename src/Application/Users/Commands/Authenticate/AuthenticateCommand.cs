261b8b35ec3b00622d12bc4dfb0488dd887d5370
Panagiotis <400362+panosru@users.noreply.github.com>
2020-07-27 02:53:01 +0300

    User Controller

--- src/Application/Users/Commands/Authenticate/AuthenticateCommand.cs
+++ src/Application/Users/Commands/Authenticate/AuthenticateCommand.cs
@@ -1,4 +1,30 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using MediatR;
+
+namespace CleanArchitecture.Application.Users.Commands.Authenticate
 {
-  public class $CLASS$ {$END$}
+    public class AuthenticateCommand : IRequest<object>
+    {
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+    }
+
+    public class AuthenticateCommandHandler : IRequestHandler<AuthenticateCommand, object>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthenticateCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<object> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        {
+            return await _identityService.Authenticate(request.UserName, request.Password);
+        }
+    }
 }
