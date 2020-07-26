7419212c0db4d69842deff09547240c7db754c8e
Panagiotis <400362+panosru@users.noreply.github.com>
2020-07-27 02:52:11 +0300

    - Use PostgreSQL - Removed Identity Server - Added JWT token generation via authentication end-point - Minor fixes

--- src/Application/Common/Interfaces/IIdentityService.cs
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -5,6 +5,8 @@ namespace CleanArchitecture.Application.Common.Interfaces
 {
     public interface IIdentityService
     {
+        Task<object> Authenticate(string username, string password);
+
         Task<string> GetUserNameAsync(string userId);
 
         Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
