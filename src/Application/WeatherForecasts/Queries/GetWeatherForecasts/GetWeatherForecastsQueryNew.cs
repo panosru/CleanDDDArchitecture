42efa4a474226bde00c2440aa14e16aab980071e
Panagiotis <400362+panosru@users.noreply.github.com>
2020-07-27 12:02:55 +0300

    API endpoints require Authorization by default

--- src/Application/WeatherForecasts/Queries/GetWeatherForecasts/GetWeatherForecastsQueryNew.cs
+++ src/Application/WeatherForecasts/Queries/GetWeatherForecasts/GetWeatherForecastsQueryNew.cs
@@ -7,18 +7,19 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecture.Application.WeatherForecasts.Queries.GetWeatherForecasts
 {
-    public class GetWeatherForecastsQuery : IRequest<IEnumerable<WeatherForecast>>
+    public class GetWeatherForecastsQueryNew : IRequest<IEnumerable<WeatherForecast>>
     {
     }
 
-    public class GetWeatherForecastsQueryHandler : IRequestHandler<GetWeatherForecastsQuery, IEnumerable<WeatherForecast>>
+    public class GetWeatherForecastsQueryNewHandler : IRequestHandler<GetWeatherForecastsQueryNew, IEnumerable<WeatherForecast>>
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing v1.1", "Bracing v1.1", "Chilly v1.1", "Cool v1.1", "Mild v1.1", 
+            "Warm v1.1", "Balmy v1.1", "Hot v1.1", "Sweltering v1.1", "Scorching v1.1"
         };
 
-        public Task<IEnumerable<WeatherForecast>> Handle(GetWeatherForecastsQuery request, CancellationToken cancellationToken)
+        public Task<IEnumerable<WeatherForecast>> Handle(GetWeatherForecastsQueryNew request, CancellationToken cancellationToken)
         {
             var rng = new Random();
 
