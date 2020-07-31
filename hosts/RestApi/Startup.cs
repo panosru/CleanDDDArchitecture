5971a4f4e8d68ac47d4cbcc99d803722b07b1986
Panagiotis <400362+panosru@users.noreply.github.com>
2020-07-31 21:55:23 +0300

    RestApi client moved to hosts/RestApi

---
+++ hosts/RestApi/Startup.cs
@@ -0,0 +1,125 @@
+using System;
+using CleanArchitecture.Application;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Infrastructure;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.REST.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using CleanArchitecture.API;
+using CleanArchitecture.REST.Utils.Swagger;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CleanArchitecture.REST
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            
+            services.Configure<SwaggerSettings>(Configuration.GetSection(nameof(SwaggerSettings)));
+
+            services
+                .AddInfrastructure(Configuration)
+                .AddApplication()
+                .AddAPI()
+                .AddRazorPages()
+                .AddMvcOptions(options =>
+                    options.Filters.Add(new AuthorizeFilter()));
+
+            services
+                .AddAuthorization()
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            services
+                .AddApiVersionWithExplorer()
+                .AddSwaggerOptions()
+                .AddSwaggerGen();
+            
+            services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+            services.AddHttpContextAccessor();
+
+            services
+                .AddHealthChecks()
+                .AddDbContextCheck<ApplicationDbContext>();
+
+            // services.AddControllersWithViews(options => 
+                // options.Filters.Add(new AuthorizeFilter()));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app, 
+            IWebHostEnvironment env, 
+            IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHealthChecks("/health");
+            
+            app.UseSwaggerDocuments();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                ServeUnknownFileTypes = true,
+                DefaultContentType = "application/yaml"
+            });
+
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints
+                    .MapDefaultControllerRoute()
+                    .RequireAuthorization();
+            });
+        }
+    }
+}
