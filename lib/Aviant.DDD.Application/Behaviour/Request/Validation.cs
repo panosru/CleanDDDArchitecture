17e969ff113cda063454beffc1cda98693d6bd33
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-01 21:07:43 +0300

    Added behaviours and interfaces in Aviant.DDD.Application library

---
+++ lib/Aviant.DDD.Application/Behaviour/Request/Validation.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace Aviant.DDD.Application.Behaviour.Request
+{
+    public class Validation<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public Validation(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(
+                    _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                
+                var failures = validationResults.SelectMany(r => r.Errors)
+                    .Where(f => f != null).ToList();
+                
+                if (0 != failures.Count)
+                    throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
