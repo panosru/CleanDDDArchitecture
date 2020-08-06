50ac60180c0ef66180388f6aff27d160b0407cd7
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-06 14:28:43 +0300

    fixed FromRoute placing

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -15,6 +15,7 @@
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            
             services.AddTransient(typeof(IPipelineBehavior<,>),
                 typeof(Performance<,>));
 
