45a29650c748facad83a45a83e6bf9fc1dfbd314
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-11 23:06:07 +0300

    EventsBase removed and now exists as HaveEvents

--- lib/Aviant.DDD.Domain/Entities/EntityBase.cs
+++ lib/Aviant.DDD.Domain/Entities/EntityBase.cs
@@ -3,9 +3,9 @@ namespace Aviant.DDD.Domain.Entities
     using System.ComponentModel.DataAnnotations;
     using Events;
 
-    public abstract class EntityBase<T> : HasEvents, IEntity
+    public abstract class EntityBase<T> : HaveEvents, IEntity
     {
         [Key]
-        public T Id { get; set; }
+        public T Id { get; set; } = default!;
     }
 }
