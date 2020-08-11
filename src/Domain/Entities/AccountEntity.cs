e650abfe444e523507cafd4fe82271c9dc8828a4
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-11 23:33:01 +0300

    Removed duplicate property from Entities

--- src/Domain/Entities/AccountEntity.cs
+++ src/Domain/Entities/AccountEntity.cs
@@ -5,8 +5,6 @@ namespace CleanDDDArchitecture.Domain.Entities
 
     public class AccountEntity : EntityBase<int>
     {
-        public int Id { get; set; }
-
         public Guid UserId { get; set; }
     }
 }
