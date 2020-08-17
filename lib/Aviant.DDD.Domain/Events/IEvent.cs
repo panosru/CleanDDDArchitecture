eb723cb557a011fc5d42d0ded27bd4e43074857b
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-17 19:55:49 +0300

    Event Occurred property populated from EventDispatcher using IDateTimeService

--- lib/Aviant.DDD.Domain/Events/IEvent.cs
+++ lib/Aviant.DDD.Domain/Events/IEvent.cs
@@ -1,8 +1,10 @@
 namespace Aviant.DDD.Domain.Events
 {
+    using System;
     using MediatR;
 
-    public interface IEvent : INotification //TODO: Revisit
+    public interface IEvent : INotification
     {
+        public DateTime Occured { get; set; }
     }
 }
