fcd5e999aabd75f397f61683a15e74b0df935f7b
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-18 00:34:08 +0300

    All entities have a Validate method

--- lib/Aviant.DDD.Domain/Entities/IEntity.cs
+++ lib/Aviant.DDD.Domain/Entities/IEntity.cs
@@ -1,7 +1,11 @@
 namespace Aviant.DDD.Domain.Entities
 {
+    using System.Threading.Tasks;
+
     public interface IEntity<T>
     {
         public T Id { get; set; }
+
+        Task<bool> Validate();
     }
 }
