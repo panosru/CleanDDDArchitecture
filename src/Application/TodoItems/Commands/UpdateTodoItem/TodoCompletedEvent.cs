7b1544f1bd4388a11884b2e1b6ade46a32032934
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-18 23:48:53 +0300

    Refactored UpdateTodoItemCommand, added Pre/Post Handler Processor examples

--- src/Application/TodoItems/Commands/UpdateTodoItem/TodoCompletedEvent.cs
+++ src/Application/TodoItems/Commands/UpdateTodoItem/TodoCompletedEvent.cs
@@ -4,16 +4,15 @@ namespace CleanDDDArchitecture.Application.TodoItems.Commands.UpdateTodoItem
     using System.Threading;
     using System.Threading.Tasks;
     using Aviant.DDD.Domain.Events;
-    using Domain.Entities;
 
     public class TodoCompletedEvent : EventBase
     {
-        public TodoCompletedEvent(TodoItemEntity completedTodo)
+        public TodoCompletedEvent(TodoItemDto completedTodo)
         {
             CompletedTodo = completedTodo;
         }
 
-        public TodoItemEntity CompletedTodo { get; }
+        public TodoItemDto CompletedTodo { get; }
     }
 
     public class TodoCompletedEventHandler : EventHandlerBase<TodoCompletedEvent>
