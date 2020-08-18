2c4a7961bbd7c0474ef9f8618131caea06f7cec6
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-18 23:29:52 +0300

    account entity implements IActivationAudited

--- src/Domain/Entities/AccountEntity.cs
+++ src/Domain/Entities/AccountEntity.cs
@@ -3,8 +3,13 @@ namespace CleanDDDArchitecture.Domain.Entities
     using System;
     using Aviant.DDD.Domain.Entities;
 
-    public class AccountEntity : EntityBase<int>
+    public class AccountEntity : EntityBase<int>,
+        IActivationAudited
     {
         public Guid UserId { get; set; }
+
+        public bool IsActive { get; set; } = true;
+        
+        public Guid? ActivationStatusModifiedBy { get; set; }
     }
 }
