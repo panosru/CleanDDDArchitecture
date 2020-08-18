253096d009c83da17359dc9e229ad4199032f53e
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-19 01:12:32 +0300

    Application DbContext Base Interface (Read & Write) implement IDisposable

--- lib/Aviant.DDD.Application/Persistance/IApplicationDbContextBase.cs
+++ lib/Aviant.DDD.Application/Persistance/IApplicationDbContextBase.cs
@@ -1,9 +1,10 @@
 namespace Aviant.DDD.Application.Persistance
 {
+    using System;
     using System.Threading;
     using System.Threading.Tasks;
 
-    public interface IApplicationDbContextBase
+    public interface IApplicationDbContextBase : IDisposable
     {
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
     }
