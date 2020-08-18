c823b3aefca7ff15c7e27d489ba6a7aa0864a2f9
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-18 23:30:09 +0300

    TodoListEntity overrides Validate method for demo purposes

--- src/Domain/Entities/TodoListEntity.cs
+++ src/Domain/Entities/TodoListEntity.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Aviant.DDD.Domain.Entities;
+    using Aviant.DDD.Domain.Validators;
 
     public class TodoListEntity : EntityBase<int>, 
         ICreationAudited, IModificationAudited, IDeletionAudited, ISoftDelete
@@ -26,5 +28,16 @@
         public Guid? LastModifiedBy { get; set; }
         
         public bool IsDeleted { get; set; }
+
+        public override Task<bool> Validate()
+        {
+            var satisfied = AssertionsConcernValidator.IsSatisfiedBy(
+                AssertionsConcernValidator.IsGreaterThan(
+                    Title.Length,
+                    5,
+                    "Title must have more than 5 chars"));
+            
+            return Task.FromResult(satisfied);
+        }
     }
 }
