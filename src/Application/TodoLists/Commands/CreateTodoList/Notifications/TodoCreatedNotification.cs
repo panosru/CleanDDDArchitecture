82129f91c1bf7b50d610de68037f0d507cde6582
Panagiotis <400362+panosru@users.noreply.github.com>
2020-08-29 11:37:00 +0300

    Replaced previous Event term with current Notification term

---
+++ src/Application/TodoLists/Commands/CreateTodoList/Notifications/TodoCreatedNotification.cs
@@ -0,0 +1,22 @@
+namespace CleanDDDArchitecture.Application.TodoLists.Commands.CreateTodoList.Notifications
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Aviant.DDD.Application.Notifications;
+
+    public class TodoCreatedNotification : Notification
+    {
+        public string Name { get; set; }
+    }
+
+    public class TodoCreatedNotificationHandler : NotificationHandler<TodoCreatedNotification>
+    {
+        public override Task Handle(TodoCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(notification.Name);
+
+            return Task.CompletedTask;
+        }
+    }
+}
