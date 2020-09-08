5bec3b5db9d8f87be6b26cac5e42295835b66474
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-08 13:52:02 +0300

    Added Weather Domain

---
+++ Domains/Weather/Hosts/RestApi/Application/Controllers/V1_0/WeatherForecast.cs
@@ -0,0 +1,55 @@
+﻿namespace CleanDDDArchitecture.Domains.Weather.Hosts.RestApi.Application.Controllers.V1_0
+{
+    using System;
+    using System.Threading.Tasks;
+    using Aviant.DDD.Application.Orchestration;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Weather.Application.UseCases.AddCity;
+    using Weather.Application.UseCases.Forecast;
+    using Weather.Application.UseCases.SyncWeatherService;
+
+    /// <summary>
+    ///     Weather endpoints
+    /// </summary>
+    public class WeatherForecast : ApiController
+    {
+        /// <summary>
+        ///     Get weather
+        /// </summary>
+        /// <returns>weather</returns>
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(RequestResult), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Get()
+        {
+            RequestResult requestResult = await Orchestrator.SendQuery(new GetWeatherForecastsQuery());
+            
+            if (!requestResult.Success)
+                return BadRequest(requestResult.Messages);
+
+            return Ok(requestResult.Payload());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Post([FromBody] SyncWeatherServiceCommand command)
+        {
+            RequestResult requestResult = await Orchestrator.SendCommand(command);
+            
+            return Ok(requestResult.Payload());
+        }
+
+        [HttpPost("[action]")]
+        [AllowAnonymous]
+        public async Task<IActionResult> AddCity([FromBody] AddCityCommand command)
+        {
+            RequestResult requestResult = await Orchestrator.SendCommand(command);
+
+            return Ok(requestResult.Payload());
+        }
+    }
+}
---
+++ Domains/Weather/Hosts/RestApi/Application/Controllers/V1_1/WeatherForecast.cs
@@ -0,0 +1,34 @@
+﻿namespace CleanDDDArchitecture.Domains.Weather.Hosts.RestApi.Application.Controllers.V1_1
+{
+    using System.Threading.Tasks;
+    using Aviant.DDD.Application.Orchestration;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Weather.Application.UseCases.Forecast;
+
+    /// <summary>
+    ///     Weather endpoints
+    /// </summary>
+    public class WeatherForecast : ApiController
+    {
+        /// <summary>
+        ///     Get weather
+        /// </summary>
+        /// <returns>weather</returns>
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(RequestResult), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Get()
+        {
+            RequestResult requestResult = await Orchestrator.SendQuery(new GetWeatherForecastsQueryNew());
+
+            if (!requestResult.Success)
+                return BadRequest(requestResult.Messages);
+
+            return Ok(requestResult.Payload());
+        }
+    }
+}
