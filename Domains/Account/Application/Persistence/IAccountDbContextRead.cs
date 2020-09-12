8b406db5ab5df20503ee3fb4ebad4ca2bfd52d3a
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-12 18:42:01 +0300

    AccountEntity is removed because it is not needed

--- Domains/Account/Application/Persistence/IAccountDbContextRead.cs
+++ Domains/Account/Application/Persistence/IAccountDbContextRead.cs
@@ -3,13 +3,9 @@ namespace CleanDDDArchitecture.Domains.Account.Application.Persistence
     #region
 
     using Aviant.DDD.Application.Persistance;
-    using Core.Entities;
-    using Microsoft.EntityFrameworkCore;
 
     #endregion
 
     public interface IAccountDbContextRead : IDbContextRead
-    {
-        DbSet<AccountEntity> Accounts { get; set; }
-    }
+    { }
 }
