9c7476b6a9100189b5853c796d27980dbf6c2433
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-18 14:14:06 +0300

    Core ApiController cupports ApiController<UseCase> specifically for UseCase handling

--- Domains/Account/Hosts/RestApi/Application/ApiController.cs
+++ Domains/Account/Hosts/RestApi/Application/ApiController.cs
@@ -2,12 +2,20 @@ namespace CleanDDDArchitecture.Domains.Account.Hosts.RestApi.Application
 {
     using Account.Application.Aggregates;
     using Aviant.DDD.Application.Orchestration;
+    using Aviant.DDD.Application.UseCases;
     using Microsoft.Extensions.DependencyInjection;
-    using ApiControllerCore = CleanDDDArchitecture.Hosts.RestApi.Core.Controllers.ApiController;
 
-    public abstract class ApiController : ApiControllerCore
+    public abstract class ApiController : CleanDDDArchitecture.Hosts.RestApi.Core.Controllers.ApiController
     {
         protected new IOrchestrator<AccountAggregate, AccountAggregateId> Orchestrator =>
             HttpContext.RequestServices.GetRequiredService<IOrchestrator<AccountAggregate, AccountAggregateId>>();
+    }
+    
+    public class ApiController<TUseCase> : CleanDDDArchitecture.Hosts.RestApi.Core.Controllers.ApiController<TUseCase>
+        where TUseCase : class, IUseCase
+    {
+        public ApiController(TUseCase useCase)
+            : base(useCase)
+        { }
     }
 }
--- Domains/Weather/Hosts/RestApi/Application/ApiController.cs
+++ Domains/Weather/Hosts/RestApi/Application/ApiController.cs
@@ -1,7 +1,15 @@
 namespace CleanDDDArchitecture.Domains.Weather.Hosts.RestApi.Application
 {
-    using ApiControllerCore = CleanDDDArchitecture.Hosts.RestApi.Core.Controllers.ApiController;
+    using Aviant.DDD.Application.UseCases;
 
-    public class ApiController : ApiControllerCore
+    public class ApiController : CleanDDDArchitecture.Hosts.RestApi.Core.Controllers.ApiController
     { }
+    
+    public class ApiController<TUseCase> : CleanDDDArchitecture.Hosts.RestApi.Core.Controllers.ApiController<TUseCase>
+        where TUseCase : class, IUseCase
+    {
+        public ApiController(TUseCase useCase)
+            : base(useCase)
+        { }
+    }
 }
