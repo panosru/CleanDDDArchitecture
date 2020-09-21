f233b3387a28041e1f81097b7f05545b29bbb709
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-21 03:15:58 +0300

    Added Api Conventions for Open API compliance

---
+++ Hosts/RestApi/Core/Controllers/ApiConventions.cs
@@ -0,0 +1,136 @@
+namespace CleanDDDArchitecture.Hosts.RestApi.Core.Controllers
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+    [SuppressMessage("Ignore", "CA1801:Ignore", MessageId = "Ignore")]
+    public static class ApiConventions
+    {
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public static void Create([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public static void Delete([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public static void Edit([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id, [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public static void Find([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public static void Get([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(200)]
+        public static void List([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public static void Post([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public static void Patch([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id, [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        {
+            // Convention
+        }
+
+        /// <summary>
+        /// </summary>
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public static void Update([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id, [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        {
+            // Convention
+        }
+    }
+}
