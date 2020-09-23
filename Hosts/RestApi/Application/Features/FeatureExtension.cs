458ea2a30aa8941f096bf9183360e6459347c7ed
Panagiotis <400362+panosru@users.noreply.github.com>
2020-09-23 17:49:43 +0300

    Added Feature Management

---
+++ Hosts/RestApi/Application/Features/FeatureExtension.cs
@@ -0,0 +1,26 @@
+namespace CleanDDDArchitecture.Hosts.RestApi.Application.Features
+{
+    using Aviant.DDD.Infrastructure.CrossCutting;
+    using Core.Features;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.FeatureManagement;
+
+    public static class FeatureExtension
+    {
+        public static IServiceCollection AddFeatureFlags(this IServiceCollection services)
+        {
+            services.AddFeatureManagement(DependencyInjectionRegistry.DefaultConfiguration);
+
+            var featureManager = services.BuildServiceProvider()
+               .GetRequiredService<IFeatureManager>();
+
+            services.AddControllersWithViews()
+               .ConfigureApplicationPartManager(
+                    apm =>
+                        apm.FeatureProviders.Add(
+                            new CustomControllerFeatureProvider(featureManager)));
+
+            return services;
+        }
+    }
+}
---
+++ Hosts/RestApi/Core/Features/CustomControllerFeatureProvider.cs
@@ -0,0 +1,75 @@
+namespace CleanDDDArchitecture.Hosts.RestApi.Core.Features
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using AutoMapper.Internal;
+    using Microsoft.AspNetCore.Mvc.ApplicationParts;
+    using Microsoft.AspNetCore.Mvc.Controllers;
+    using Microsoft.FeatureManagement;
+    using Microsoft.FeatureManagement.Mvc;
+
+    public class CustomControllerFeatureProvider
+        : IApplicationFeatureProvider<ControllerFeature>
+    {
+        private readonly IFeatureManager _featureManager;
+
+        private ControllerFeature? _feature;
+
+        public CustomControllerFeatureProvider(IFeatureManager featureManager) => _featureManager = featureManager;
+
+        #region IApplicationFeatureProvider<ControllerFeature> Members
+
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
+        {
+            _feature = feature;
+
+            feature.Controllers
+               .Select((controller, index) => (controller, index))
+               .ToList()
+               .ForEach(
+                    x =>
+                        GetCustomAttributes(x.controller, x.index));
+        }
+
+        #endregion
+
+        private void GetCustomAttributes(TypeInfo controller, int index)
+        {
+            controller.AsType()
+               .CustomAttributes
+               .ForAll(
+                    customAttribute =>
+                        FilterCustomAttributes(customAttribute, index));
+        }
+
+
+        private void FilterCustomAttributes(CustomAttributeData customAttribute, int index)
+        {
+            if (customAttribute.AttributeType.FullName != typeof(FeatureGateAttribute).FullName)
+                return;
+
+            if (customAttribute.ConstructorArguments.First().Value is IEnumerable arguments)
+                arguments.Cast<object>()
+                   .ForAll(
+                        @object =>
+                            RemoveDisabledFeatureFromControllers(@object, index));
+        }
+
+        private void RemoveDisabledFeatureFromControllers(object argumentValue, int index)
+        {
+            var typedArgument      = (CustomAttributeTypedArgument) argumentValue!;
+            var typedArgumentValue = (Feature) (int) typedArgument.Value!;
+
+            var isFeatureEnabled = _featureManager
+               .IsEnabledAsync(typedArgumentValue.ToString())
+               .ConfigureAwait(false)
+               .GetAwaiter()
+               .GetResult();
+
+            if (!isFeatureEnabled)
+                _feature?.Controllers.RemoveAt(index);
+        }
+    }
+}
