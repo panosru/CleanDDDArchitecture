a7687d210ba0bae24a6276b08d29fba2f4e80686
Panagiotis <400362+panosru@users.noreply.github.com>
2020-10-08 18:58:19 +0300

    Fixed missing packages

--- Domains/Account/CrossCutting/AccountCrossCutting.cs
+++ Domains/Account/CrossCutting/AccountCrossCutting.cs
@@ -2,6 +2,7 @@ namespace CleanDDDArchitecture.Domains.Account.CrossCutting
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using System.Threading.Tasks;
     using Application.Aggregates;
