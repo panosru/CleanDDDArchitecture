6d775c57ad3e71a50f767113a061d6015e5ab016
Panagiotis <400362+panosru@users.noreply.github.com>
2020-10-14 18:33:40 +0300

    ExceptionRaised Event refactored

--- Hosts/RestApi/Application/Events/ExceptionRaised.cs
+++ Hosts/RestApi/Application/Events/ExceptionRaised.cs
@@ -3,34 +3,43 @@ namespace CleanDDDArchitecture.Hosts.RestApi.Application.Events
     using System;
     using System.Threading;
     using System.Threading.Tasks;
-    using MediatR;
+    using Aviant.DDD.Application.ApplicationEvents;
 
-    public sealed class ExceptionRaised : IRequest //TODO: Move to Application exceptions event?
+    internal sealed class ExceptionRaised : ApplicationEvent
     {
+        public readonly string AssemblyToBlame;
+
         /// <summary>
         /// </summary>
         // ReSharper disable once MemberCanBeInternal
-        public readonly string Error;
+        public readonly string ErrorMessage;
 
-        /// <summary>
-        /// </summary>
-        /// <param name="error"></param>
-        internal ExceptionRaised(string error) => Error = error;
+        public readonly string StackTrace;
+
+        public readonly string SerializedException;
+
+        public ExceptionRaised(
+            string   assemblyToBlame,
+            string   errorMessage,
+            string   stackTrace,
+            string   serializedException)
+        {
+            AssemblyToBlame     = assemblyToBlame;
+            ErrorMessage        = errorMessage;
+            StackTrace          = stackTrace;
+            SerializedException = serializedException;
+        }
     }
 
-    public sealed class ExceptionRaisedHandler : IRequestHandler<ExceptionRaised>
+    internal sealed class ExceptionRaisedHandler : ApplicationEventHandler<ExceptionRaised>
     {
-        #region IRequestHandler<ExceptionRaised> Members
-
-        public Task<Unit> Handle(ExceptionRaised request, CancellationToken cancellationToken)
+        public override Task Handle(ExceptionRaised request, CancellationToken cancellationToken)
         {
             Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%");
-            Console.WriteLine(request.Error);
+            Console.WriteLine(request.ErrorMessage);
             Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%");
 
-            return Task.FromResult(Unit.Value);
+            return Task.CompletedTask;
         }
-
-        #endregion
     }
 }
