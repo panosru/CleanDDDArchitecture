c24e5f4bb083bd92877454c77719f1d957bd6942
Panagiotis <400362+panosru@users.noreply.github.com>
2021-08-22 11:59:58 +0300

    Added check for mandatory claims

---
+++ Domains/Account/Core/Exceptions/IdentityException.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanDDDArchitecture.Domains.Account.Core.Exceptions
+{
+    using System.Net;
+    using Aviant.DDD.Core.Exceptions;
+
+    public sealed class IdentityException : CoreException
+    {
+        public IdentityException(string message, HttpStatusCode errorCode)
+            : base(message, (int)errorCode)
+        { }
+    }
+}
