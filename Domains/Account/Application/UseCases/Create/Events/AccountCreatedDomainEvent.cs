97e21a02ca62a30b7e1a7d3e9169558ba28a5708
Panagiotis <400362+panosru@users.noreply.github.com>
2021-08-26 21:39:30 +0300

    Merge event consumer(s) into nested classes (the code looks better)

--- Domains/Account/Application/UseCases/Create/Events/AccountCreatedDomainEvent.cs
+++ Domains/Account/Application/UseCases/Create/Events/AccountCreatedDomainEvent.cs
@@ -3,8 +3,13 @@
 namespace CleanDDDArchitecture.Domains.Account.Application.UseCases.Create.Events
 {
     using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Aggregates;
     using Aviant.DDD.Core.DomainEvents;
+    using Aviant.DDD.Core.EventBus;
+    using Identity;
+    using Microsoft.AspNetCore.Identity;
 
     public sealed class AccountCreatedDomainEvent : DomainEvent<AccountAggregate, AccountAggregateId>
     {
@@ -42,5 +47,44 @@ namespace CleanDDDArchitecture.Domains.Account.Application.UseCases.Create.Event
         public IEnumerable<string> Roles { get; private set; }
 
         public bool EmailConfirmed { get; private set; }
+
+        #region Nested type: AccountCreatedDomainEventConsumer
+
+        internal sealed class AccountCreatedDomainEventConsumer : DomainEventHandler<AccountCreatedDomainEvent>
+        {
+            private readonly UserManager<AccountUser> _userManager;
+
+            public AccountCreatedDomainEventConsumer(UserManager<AccountUser> userManager) =>
+                _userManager = userManager;
+
+            public override async Task Handle(
+                EventReceived<AccountCreatedDomainEvent> @event,
+                CancellationToken                        cancellationToken)
+            {
+                AccountUser user = new()
+                {
+                    Id             = @event.Event.Id.Key,
+                    UserName       = @event.Event.UserName,
+                    Email          = @event.Event.Email,
+                    FirstName      = @event.Event.FirstName,
+                    LastName       = @event.Event.LastName,
+                    EmailConfirmed = @event.Event.EmailConfirmed
+                };
+
+                // It is the responsibility of previous steps validators to make sure that the creation of the user
+                // will not fail (for example password property rules etc.)
+                IdentityResult userResult = await _userManager.CreateAsync(
+                        user,
+                        @event.Event.Password)
+                   .ConfigureAwait(false);
+
+                if (userResult.Succeeded)
+                    await _userManager.AddToRolesAsync(user, @event.Event.Roles);
+                // else
+                //TODO: raise failed event
+            }
+        }
+
+        #endregion
     }
 }
