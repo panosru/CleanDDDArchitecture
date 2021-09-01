ae79d60970f32998054640fb9a137c50a4ec7593
Panagiotis <400362+panosru@users.noreply.github.com>
2021-09-01 15:50:41 +0300

    Added Shared Domain and implement Weather Sync Job

---
+++ Domains/Shared/Core/JobQueue.cs
@@ -0,0 +1,13 @@
+namespace CleanDDDArchitecture.Domains.Shared.Core
+{
+    public static class JobQueue
+    {
+        public const string Default = "default";
+
+        public const string Main = "main";
+
+        public const string Second = "second";
+
+        public const string Third = "third";
+    }
+}
