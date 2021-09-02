8c5eee5175893b1e4b101e6c3d5a9ba6cb091730
Panagiotis <400362+panosru@users.noreply.github.com>
2021-09-03 01:27:54 +0300

    Roles and hangfire authorization improvement

---
+++ Domains/Shared/Core/Identity/Roles.cs
@@ -0,0 +1,19 @@
+namespace CleanDDDArchitecture.Domains.Shared.Core.Identity
+{
+    public sealed class Roles
+    {
+        public const string Root = "root";
+
+        public const string SuperAdmin = "superadmin";
+
+        public const string Admin = "admin";
+
+        public const string Manager = "manager";
+
+        public const string Moderator = "moderator";
+
+        public const string Member = "member";
+
+        public const string Guest = "guest";
+    }
+}
