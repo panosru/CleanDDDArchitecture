1b8a1cf3be612e3f8af2826263d0126d835a8732
Panagiotis <400362+panosru@users.noreply.github.com>
2021-09-12 02:51:54 +0300

    Remove logger from Razor Pages

--- Hosts/WebApp/Application/Pages/Index.cshtml.cs
+++ Hosts/WebApp/Application/Pages/Index.cshtml.cs
@@ -1,14 +1,9 @@
 ﻿namespace CleanDDDArchitecture.Hosts.WebApp.Application.Pages
 {
     using Microsoft.AspNetCore.Mvc.RazorPages;
-    using Microsoft.Extensions.Logging;
 
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger) => _logger = logger;
-
         public void OnGet()
         { }
     }
