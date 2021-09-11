4c6876dc7215e31c9099c16e99d2a103801cf40b
Panagiotis <400362+panosru@users.noreply.github.com>
2021-09-12 02:51:54 +0300

    Remove logger from Razor Pages

--- Hosts/WebApp/Application/Pages/Privacy.cshtml.cs
+++ Hosts/WebApp/Application/Pages/Privacy.cshtml.cs
@@ -1,14 +1,9 @@
 ﻿namespace CleanDDDArchitecture.Hosts.WebApp.Application.Pages
 {
     using Microsoft.AspNetCore.Mvc.RazorPages;
-    using Microsoft.Extensions.Logging;
 
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger) => _logger = logger;
-
         public void OnGet()
         { }
     }
