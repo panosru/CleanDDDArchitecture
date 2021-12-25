ccf6de99c0364598b4706f5fde4bccfe1ef8219c
Panagiotis <400362+panosru@users.noreply.github.com>
2021-12-25 03:06:07 +0200

    Added docker support

---
+++ docker/services/Postgres.Dockerfile
@@ -0,0 +1,3 @@
+FROM postgres:latest
+RUN localedef -i en_GB -c -f UTF-8 -A /usr/share/locale/locale.alias en_GB.UTF-8
+ENV LANG en_GB.utf8
---
+++ docker/services/postgres.init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE "CleanDDDArchitecture"
+    WITH
+    OWNER = panosru
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_GB.utf8'
+    LC_CTYPE = 'en_GB.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE "Hangfire"
+    WITH
+    OWNER = panosru
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_GB.utf8'
+    LC_CTYPE = 'en_GB.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
