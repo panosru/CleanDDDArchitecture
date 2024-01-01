daf2ac3de740889f8b6c643899d1a129da5d4a71
Panagiotis <400362+panosru@users.noreply.github.com>
2024-01-01 13:59:42 +0200

    Remove Duente IdentityServer and replace it with the new .Net 8 approach

--- Domains/Account/Infrastructure/Persistence/Contexts/AccountDbContextWrite.cs
+++ Domains/Account/Infrastructure/Persistence/Contexts/AccountDbContextWrite.cs
@@ -3,18 +3,13 @@ namespace CleanDDDArchitecture.Domains.Account.Infrastructure.Persistence.Contex
 using Application.Identity;
 using Application.Persistence;
 using Aviant.Infrastructure.Identity.Persistence.Contexts;
-using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 public sealed class AccountDbContextWrite
     : AuthorizationDbContextWrite<AccountDbContextWrite, AccountUser, AccountRole>, IAccountDbContextWrite
 {
     public AccountDbContextWrite(
-        DbContextOptions<AccountDbContextWrite> options,
-        IOptions<OperationalStoreOptions>       operationalStoreOptions)
-        : base(
-            options,
-            operationalStoreOptions)
+        DbContextOptions<AccountDbContextWrite> options)
+        : base(options)
     { }
 }
